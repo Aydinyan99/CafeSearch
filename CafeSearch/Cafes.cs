58907308fa91b58e566bec537f285a7ed4106ccc
User <User@WIN-1JG2DN1BSSN>
2017-05-02 10:56:07 +0400

    ...

--- CafeSearch/Cafes.cs
+++ CafeSearch/Cafes.cs
@@ -8,25 +8,49 @@ namespace CafeSearch
 {
     class Cafes
     {
-    
+
         public static List<Cafe> cafes = new List<Cafe>();
         public static int theNumberOfCafes = cafes.Count;
-
-        public  string cafesOpenNow()
+        public Cafes()
         {
-            string cafesOpenNow="";
+            cafes.Add(cafe1);
+            cafes.Add(cafe2);
+            cafes.Add(cafe3);
+            cafes.Add(cafe4);
+        }
+        public string AllCafes()
+        {
+            string AllCafes = "";
+            foreach (Cafe cafe in cafes)
+            {
+                AllCafes += cafe;
+            }
+            return AllCafes;
+        }
+        public string CafesOpenNow()
+        {
+            string cafesOpenNow = "";
             foreach (Cafe cafe in cafes)
             {
                 if (DateTime.Now.TimeOfDay >= cafe.OpenHour && DateTime.Now.TimeOfDay <= cafe.CloseHour)
-                    cafesOpenNow = cafesOpenNow + (cafe);
+                    cafesOpenNow += cafe;
             }
             return cafesOpenNow;
         }
-
+        public string CafesWithWifi()
+        {
+            string cafesWithWifi = "";
+            foreach (Cafe cafe in cafes)
+            {
+                if (cafe.WifiAvailability)
+                    cafesWithWifi += cafe;
+            }
+            return cafesWithWifi;
+        }
         public void AddCafe(Cafe cafe)
         {
             cafes.Add(cafe);
-        } 
+        }
         public Cafe GetCafeByName(string name)
         {
             foreach (Cafe var in cafes)
@@ -39,7 +63,6 @@ namespace CafeSearch
         public void GetCafesByDecliningRating()
         {
             List<Cafe> cafeRating = cafes;
-            float rating = 5;
             for (int i = 0; i < cafes.Count; i++)
             {
                 for (int j = 0; j < cafes.Count - i - 1; j++)
@@ -71,6 +94,47 @@ namespace CafeSearch
         {
             return new List<Cafe>();
         }
-       
+        Cafe cafe1 = new Cafe()
+        {
+            Name = "Tumanyan Shaurma",
+            Address = "Tumanyan Street",
+            OpenHour = new TimeSpan(09, 00, 00),
+            CloseHour = new TimeSpan(20, 00, 00),
+            Rating = 4f,
+            OfficialWebsite = "http://tshaurma.com",
+            WifiAvailability = true
+        };
+
+        Cafe cafe2 = new Cafe()
+        {
+            Name = "Loft",
+            Address = "Moskovyan Street",
+            OpenHour = new TimeSpan(09, 00, 00),
+            CloseHour = new TimeSpan(11, 00, 00),
+            Rating = 4.7f,
+            OfficialWebsite = "http://loft.am",
+            WifiAvailability = true
+        };
+        Cafe cafe3 = new Cafe()
+        {
+            Name = "Jose",
+            Address = "Khanjyan Street",
+            OpenHour = new TimeSpan(11, 00, 00),
+            CloseHour = new TimeSpan(23, 59, 00),
+            Rating = 4.3f,
+            OfficialWebsite = "http://jose.am",
+            WifiAvailability = true,
+            Reviews = "Davit Mkoyan: Nice club-restaurant  to arrange birthdays and other events. Has live music. Is expensive.\n"
+        };
+        Cafe cafe4 = new Cafe()
+        {
+            Name = "Tashir",
+            Address = "Teryan Street",
+            OpenHour = new TimeSpan(10, 00, 00),
+            CloseHour = new TimeSpan(23, 00, 00),
+            Rating = 4.4f,
+            OfficialWebsite = "http://tashirpizza.am",
+            WifiAvailability = true
+        };
     }
 }
--- CafeSearch/Program.cs
+++ CafeSearch/Program.cs
@@ -11,152 +11,129 @@ namespace CafeSearch
         static void Main(string[] args)
         {
             Cafes cafes = new Cafes();
-            Cafe cafe1 = new Cafe()
-            {
-                Name = "Tumanyan Shaurma",
-                Address = "Tumanyan Street",
-                OpenHour = new TimeSpan(09, 00, 00),
-                CloseHour = new TimeSpan(20, 00, 00),
-                Rating = 4f,
-                OfficialWebsite = "http://tshaurma.com",
-                WifiAvailability = true
-            };
-           
-            Cafe cafe2 = new Cafe()
-            {
-                Name = "Loft",
-                Address = "Moskovyan Street",
-                OpenHour = new TimeSpan(09, 00, 00),
-                CloseHour = new TimeSpan(05, 00, 00),
-                Rating = 4.7f,
-                OfficialWebsite = "http://loft.am",
-                WifiAvailability = true
-            };
-            Cafe cafe3 = new Cafe()
-            {
-                Name = "Jose",
-                Address = "Khanjyan Street",
-                OpenHour = new TimeSpan(11, 00, 00),
-                CloseHour = new TimeSpan(24, 00, 00),
-                Rating = 4.3f,
-                OfficialWebsite = "http://jose.am",
-                WifiAvailability = true,
-                Reviews = "Davit Mkoyan: Nice club-restaurant  to arrange birthdays and other events. Has live music. Is expensive. +\n"
-            };
-            Cafe cafe4 = new Cafe()
-            {
-                Name = "Tashir",
-                Address = "Teryan Street",
-                OpenHour = new TimeSpan(10, 00, 00),
-                CloseHour = new TimeSpan(23, 00, 00),
-                Rating = 4.4f,
-                OfficialWebsite = "http://tashirpizza.am",
-                WifiAvailability = true
-            };
-            cafes.AddCafe(cafe1);
-            cafes.AddCafe(cafe2);
-            cafes.AddCafe(cafe3);
-            cafes.AddCafe(cafe4);
 
-            Console.WriteLine("Thanks for using our program! \n");
-            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine("Welcome! \n");
+            //System.Threading.Thread.Sleep(1000);
             Console.WriteLine("What do you want to know? \n");
-            ChooseAnotherCafe(cafes);
+            //ChooseAnotherCafe(cafes);
 
+            InputNumbers(cafes);
 
         }
-        public static void CafeReserve(int number, Cafes cafes, Cafe cafe)
+        public static void InputNumbers(Cafes cafes)
         {
-            if (cafe != null)
-            {
-                Console.WriteLine("\n" + cafe.ToString() + "\n");
-                        System.Threading.Thread.Sleep(1000);
-                        Console.WriteLine("Do you want to go " + cafe.Name + "? (yes/no)");
-                        string answer = Console.ReadLine();
-                        if (answer == "yes")
-                        {
-                            Console.WriteLine("When do you want to go?");
-                            Console.ReadLine();
-                            Random rand = new Random();
-                            int random = rand.Next(10);
-                            if (random < 1)
-                            {
-                                Console.WriteLine("Your table is reserved.");
-                            }
-                            else
-                            {
-                                Console.WriteLine("Sorry, all tables are reserved!");
-                                Console.WriteLine("Do you want to choose another cafe? (yes/no)");
-                                answer = Console.ReadLine();
-                                while (answer != "yes" && answer != "no")
-                                {
-                                    Console.WriteLine("Wrong input! Enter the word again.");
-                                    answer = Console.ReadLine();
-                                }
-                                if (answer == "yes")
-                                {
-                                    ChooseAnotherCafe(cafes);
-                                }
-                                else
-                                {
-                                    Console.WriteLine("Good bye!");
-                                    System.Threading.Thread.Sleep(1000);
-                                }
-                            }
-                        }
-                        else
-                        {
-                            Console.WriteLine("Do you want to choose another cafe? (yes/no)");
-                            answer = Console.ReadLine();
-                            while (answer != "yes" && answer != "no")
-                            {
-                                Console.WriteLine("Wrong input! Enter the word again.");
-                                answer = Console.ReadLine();
-                            }
-                            if (answer == "yes")
-                            {
-                                ChooseAnotherCafe(cafes);
-                            }
-                            else
-                            {
-                                Console.WriteLine("Good bye!");
-                                System.Threading.Thread.Sleep(1000);
-                            }
-                        }
-                    }
-                    else
-                    {
-                        Console.WriteLine("\nCafe is not found!");
-                    }            
-        }
-        public static void ChooseAnotherCafe(Cafes cafes)
-        {
-            System.Threading.Thread.Sleep(1000);
-            Console.WriteLine("\nYou can find: \n1. Cafes by name, 2. Cafes by address \n3. Now open cafes, 4. Cafes that have wifi\n5. All cafes on map \n");
-            System.Threading.Thread.Sleep(1000);
+            //System.Threading.Thread.Sleep(1000);
+            Console.WriteLine("You can find: \n1. Cafes by name, 2. Cafes by address \n3. Now open cafes, 4. Cafes that have wifi\n5. All cafes on map \n");
+            //System.Threading.Thread.Sleep(1000);
             Console.WriteLine("Select what you want by its number.");
-
             string numberOfFunction = Console.ReadLine();
-            int number;
             while (!(numberOfFunction.Length == 1 && Convert.ToChar(numberOfFunction) > '0' && Convert.ToChar(numberOfFunction) < '6'))
             {
                 Console.WriteLine("Wrong input! Enter the number again.");
                 numberOfFunction = Console.ReadLine();
             }
-            number = Convert.ToInt32(numberOfFunction);
-            switch (number)
+            switch (numberOfFunction)
             {
-                case 1:
-                    Console.WriteLine("Enter the name of the cafe.");
-                    Cafe cafe = cafes.GetCafeByName(Console.ReadLine());
-                    CafeReserve(number, cafes, cafe);
+                case "1":
+                    CafesByName(cafes);
                     break;
-                case 2:
-                    Console.WriteLine("Enter the address of the cafe.");
-                    cafe = cafes.GetCafeByAddress(Console.ReadLine());
-                    CafeReserve(number, cafes, cafe);
+                case "2":
+                    break;
+                case "3":
+                    break;
+                case "4":
+                    break;
+                case "5":
                     break;
             }
+
         }
+        public static void CafesByName(Cafes cafes)
+        {
+            Console.WriteLine("Enter the name of the cafe.");
+            Cafe cafe = cafes.GetCafeByName(Console.ReadLine());
+            while (cafe == null)
+            {
+                Console.WriteLine("Cafe is not found!");
+                cafe = cafes.GetCafeByName(Console.ReadLine());
+            }
+            CafeReserve(cafes, cafe);
         }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public static void CafeReserve(Cafes cafes, Cafe cafe)
+        {
+            Console.WriteLine("\n" + cafe.ToString() + "\n");
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine("Do you want to go " + cafe.Name + "? (yes/no)");
+            string answer = Console.ReadLine();
+            if (answer == "yes")
+            {
+                Console.WriteLine("When do you want to go?");
+                Console.ReadLine();
+                Random rand = new Random();
+                int random = rand.Next(10);
+                if (random < 8)
+                {
+                    Console.WriteLine("Your table is reserved.");
+                }
+                else
+                {
+                    Console.WriteLine("Sorry, all tables are reserved!");
+                    Console.WriteLine("Do you want to choose another cafe? (yes/no)");
+                    answer = Console.ReadLine();
+                    while (answer != "yes" && answer != "no")
+                    {
+                        Console.WriteLine("Wrong input! Enter the word again.");
+                        answer = Console.ReadLine();
+                    }
+                    if (answer == "yes")
+                    {
+                        InputNumbers(cafes);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Good bye!");
+                        System.Threading.Thread.Sleep(1000);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Do you want to choose another cafe? (yes/no)");
+                answer = Console.ReadLine();
+                while (answer != "yes" && answer != "no")
+                {
+                    Console.WriteLine("Wrong input! Enter the word again.");
+                    answer = Console.ReadLine();
+                }
+                if (answer == "yes")
+                {
+                    InputNumbers(cafes);
+                }
+                else
+                {
+                    Console.WriteLine("Good bye!");
+                    System.Threading.Thread.Sleep(1000);
+                }
+            }
+        }
+    }
 }
